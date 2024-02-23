b90d03317df2c84b8857a32b099a1bacdfb0fdef
tekkac <tarik@carbonable.io>
2024-02-23 13:00:19 +0100

    🔨 wip fix script

--- scripts/data_to_wave.py
+++ scripts/data_to_wave.py
@@ -1,4 +1,5 @@
 import sys
+import matplotlib.pyplot as plt
 
 if len(sys.argv) != 2:
     print("Usage: python data_to_wave.py <input_file>")
@@ -6,10 +7,16 @@ if len(sys.argv) != 2:
 
 input_file = sys.argv[1]
 
-with open(input_file, 'r') as f:
+with open(input_file) as f:
     data = f.read().splitlines()
 
-data_bytes = data[5].encode().decode('unicode-escape').encode('ISO-8859-1')
+# plt.plot("".join(data[5:-3]))
+# plt.show()
+
+data_bytes = "".join(data[5:-3])
+data_bytes = data_bytes.encode('utf-8')
+data_bytes = data_bytes.decode('unicode-escape')
+data_bytes = data_bytes.encode('ISO-8859-1')
 
 with open(input_file + '.wav', 'wb') as f:
     f.write(data_bytes)
