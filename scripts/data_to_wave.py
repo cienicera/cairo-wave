f229a2bc17987ed2b701500697092415cba77dea
tekkac <tarik@carbonable.io>
2024-04-08 16:21:11 +0200

    🔨 update to wave local script

--- scripts/data_to_wave.py
+++ scripts/data_to_wave.py
@@ -10,10 +10,12 @@ input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read().splitlines()
 
-# plt.plot("".join(data[5:-3]))
-# plt.show()
+i = 0
+while i < len(data):
+    if data[i].startswith('RIFF'): break
+    i+=1
 
-data_bytes = "".join(data[5:-3])
+data_bytes = "".join(data[i:-3])
 data_bytes = data_bytes.encode('utf-8')
 data_bytes = data_bytes.decode('unicode-escape')
 data_bytes = data_bytes.encode('ISO-8859-1')
